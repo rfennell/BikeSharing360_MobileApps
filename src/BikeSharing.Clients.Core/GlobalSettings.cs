1badd7383fffff579c3e9386afc73e12c25390de
Richard Fennell <richard@blackmarble.co.uk>
2017-03-23 10:57:53 +0000

    RF Demo settings

--- src/BikeSharing.Clients.Core/GlobalSettings.cs
+++ src/BikeSharing.Clients.Core/GlobalSettings.cs
@@ -2,19 +2,19 @@
 {
     public static class GlobalSettings
     {
-        public const string AuthenticationEndpoint = "http://YOUR_PROFILE_SERVICE.azurewebsites.net/";
-        public const string EventsEndpoint = "http://YOUR_EVENTS_SERVICE.azurewebsites.net/";
-        public const string IssuesEndpoint = "http://YOUR_ISSUES_SERVICE.azurewebsites.net/";
-        public const string RidesEndpoint = "http://YOUR_RIDES_SERVICE.azurewebsites.net/";
+        public const string AuthenticationEndpoint = "http://bikesharing-services-profiles2kttqgxlfnitk.azurewebsites.net/";
+        public const string EventsEndpoint = "http://bikesharing-services-events2kttqgxlfnitk.azurewebsites.net/";
+        public const string IssuesEndpoint = "http://bikesharing-services-feedback2kttqgxlfnitk.azurewebsites.net/";
+        public const string RidesEndpoint = "http://bikesharing-services-rides2kttqgxlfnitk.azurewebsites.net/";
 
-        public const string OpenWeatherMapAPIKey = "YOUR_WEATHERMAP_API_KEY";
+        public const string OpenWeatherMapAPIKey = "fc23a05cd393326b7c4843b73827e163";
 
-        public const string HockeyAppAPIKeyForAndroid = "YOUR_HOCKEY_APP_ID";
+        public const string HockeyAppAPIKeyForAndroid = "bbfa62da8cd74d2b9140e6e89f417154";
         public const string HockeyAppAPIKeyForiOS = "YOUR_HOCKEY_APP_ID";
 
         public const string SkypeBotAccount = "skype:YOUR_BOT_ID?chat";
 
-        public const string BingMapsAPIKey = "YOUR_BINGMAPS_API_KEY";
+        public const string BingMapsAPIKey = "AqP8gc6IGKVyO1PxQDOrKKtVeOYCBwy31yAzUd9zEQeda7kCKCJ0TVROyOimZZ1V";
 
         public static string City => "New York City";
 
